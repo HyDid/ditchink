cd7fa28c1f9c8885729221cd11673106dc2a2825
LiHongYu <2443077810@qq.com>
2016-10-30 11:22:06 +0800

    消息导航

--- ditchink/ditchink/Message/Controller/HyMessageViewController.m
+++ ditchink/ditchink/Message/Controller/HyMessageViewController.m
@@ -7,22 +7,86 @@
 //
 
 #import "HyMessageViewController.h"
+#import "HyTitleButtonView.h"
 
 @interface HyMessageViewController ()
 
+@property (nonatomic,strong)HyTitleButtonView *titleButtonView;
+@property (nonatomic,strong)UITableView *messageTableview;
+@property (nonatomic,strong)UITableView *systemTableview;
 @end
 
 @implementation HyMessageViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view from its nib.
+    
+
+    //设置导航栏
+    [self setupNav];
+    
+    UITableView *systemTableview = [[UITableView alloc]init];
+    systemTableview.frame = self.view.frame;
+    systemTableview.backgroundColor = [UIColor blueColor];
+    [self.view addSubview:systemTableview];
+    UITableView *messagetableview = [[UITableView alloc]init];
+    messagetableview.frame = self.view.frame;
+    messagetableview.backgroundColor = [UIColor greenColor];
+    [self.view addSubview:messagetableview];
+    self.systemTableview = systemTableview;
+    self.messageTableview = messagetableview;
+
+    
+
+}
+-(void)viewWillAppear:(BOOL)animated{
+    [super viewWillAppear:animated];
+    self.titleButtonView.messageButton.selected = YES;
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+-(void)setupNav{
+    
+    UIButton *titleBtn = [[UIButton alloc]initWithFrame:CGRectMake(0, 0, 30, 30)];
+    [titleBtn setImage:[UIImage imageNamed:@"icon-my"] forState:UIControlStateNormal];
+    [titleBtn setImage:[UIImage imageNamed:@"icon-my-click"] forState:UIControlStateHighlighted];
+    [titleBtn addTarget:self action:@selector(NavLeftBtn) forControlEvents:UIControlEventTouchUpInside];
+    self.navigationItem.leftBarButtonItem = [[UIBarButtonItem alloc]initWithCustomView:titleBtn];
+    
+
+    HyTitleButtonView *titleButtonView = [[HyTitleButtonView alloc]init];
+    titleButtonView.frame = CGRectMake(0, 0, 160, 30);
+    titleButtonView.backgroundColor = [UIColor redColor];
+    self.navigationItem.titleView = titleButtonView;
+    
+    self.titleButtonView = titleButtonView;
+    [self.titleButtonView.messageButton addTarget:self action:@selector(messageButtonOnclick) forControlEvents:UIControlEventTouchUpInside];
+    [self.titleButtonView.systemButton addTarget:self action:@selector(systemButtonOnclick) forControlEvents:UIControlEventTouchUpInside];
+    
+    
+
+    
+    
 }
+
+//导航栏左侧按钮
+-(void)NavLeftBtn{
+    NSLog(@"left");
+}
+
+-(void)messageButtonOnclick{
+    self.titleButtonView.messageButton.selected = YES;
+    self.titleButtonView.systemButton.selected = NO;
+    self.messageTableview.hidden = NO;
+    self.systemTableview.hidden = YES;
+    
+}
+-(void)systemButtonOnclick{
+    self.titleButtonView.messageButton.selected = NO;
+    self.titleButtonView.systemButton.selected = YES;
+    self.messageTableview.hidden = YES;
+    self.systemTableview.hidden = NO;
+}
+
 
 /*
 #pragma mark - Navigation
