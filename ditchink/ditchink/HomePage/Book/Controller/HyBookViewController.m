3c804313180dfbaa2cd541477cfc293c178007c6
LiHongYu <2443077810@qq.com>
2017-06-06 20:00:53 +0800

    图书推荐UI

--- ditchink/ditchink/HomePage/Book/Controller/HyBookViewController.m
+++ ditchink/ditchink/HomePage/Book/Controller/HyBookViewController.m
@@ -14,24 +14,40 @@
 
 @implementation HyBookViewController
 
+static int totalColumns = 3;
+
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view from its nib.
+    
+    self.view.backgroundColor = HyColor(239, 239, 239);
+    
+    CGFloat ConverW = 80;
+    CGFloat ConverH = 120;
+    CGFloat marginX = (SCREEN_WIDTH - totalColumns * ConverW) / (totalColumns + 1);
+    CGFloat marginY = 30;
+    for (int index = 0; index<7; index++) {
+        UIImageView *aView = [[UIImageView alloc]initWithImage:[UIImage imageNamed:[NSString stringWithFormat:@"icon-pic%i",index]]];
+        int row = index / totalColumns;
+        int col = index % totalColumns;
+        
+        CGFloat appX = marginX + col * (ConverW + marginX);
+        CGFloat appY = marginY + row * (ConverH + marginY);
+        aView.frame = CGRectMake(appX, appY, ConverW, ConverH);
+        
+        [self.view addSubview:aView];
+    }
+    
+    for (int index = 0; index<3; index++) {
+        UIImageView *iView = [[UIImageView alloc]initWithImage:[UIImage imageNamed:@"icon-base2"]];
+        
+        iView.bounds = CGRectMake(0, 0, SCREEN_WIDTH - 20, 10);
+        iView.center = CGPointMake(SCREEN_WIDTH/2, marginY/2 + (index + 1) *(ConverH + marginY));
+        
+        [self.view addSubview:iView];
+    }
+    
+    
+    
 }
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
